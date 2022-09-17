0040b7e01caf8ff047756139084a7888df8769a1
newargus <newargus@gmail.com>
2022-09-17 14:35:48 +0200

    Delete Arm/v6

--- BUILD.sh
+++ BUILD.sh
@@ -1,6 +1,6 @@
 VERSION=$(curl -s -XGET https://api.github.com/repos/Kozea/Radicale/tags | grep name -m 1 | awk '{print $2}' | cut -d'"' -f2 | cut -d'v' -f2)
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 \
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 \
 -f Dockerfile \
 -t newargus/radicale:latest \
 -t newargus/radicale:"${VERSION}" \
